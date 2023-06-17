1c80d7d42c62c86f536f30a653662acd1797a484
SH4ND <miqueiasantos20@gmail.com>
2023-06-17 07:51:52 -0300

    aljeter: Include script for clone ksu
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,6 +14,11 @@ echo ""
 git clone -b aljeter-337 https://github.com/Team-NoobMaster69/kernel_motorola_msm8937.git kernel/motorola/msm8937
 echo ""
 
+# KSU
+cd kernel/motorola/msm8937
+rm -rf KernelSU && curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+cd -
+
 # Dependencies
 echo ""
 git clone -b lineage-18.1 https://github.com/LineageOS/android_external_sony_boringssl-compat external/sony/boringssl-compat
